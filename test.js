ddfbb88f269b9c287b3363cddac6dec88f4ae643
Paulo Henrique <paulohenriquesantos280@gmail.com>
2026-02-20 20:21:26 -0300

    Quarto commit

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-console.log('João nasceu em 1984.');
-console.log('João conheceu Maria em 2000.');
-console.log('Maria casou-se com João em 2012.');
-console.log('Maria engravidou de João em 2015.');
-console.log('O nome do filho de João é Eduardo.');
+//Não podemos criar variáveis com palavras reservadas
+
+let nome; //declarando varável sem inicializá-la
+nome = 'João'; //iniciando variável
+
+console.log(nome, 'nasceu em 1984.');
+console.log(nome, 'conheceu Maria em 2000.');
+console.log('Maria casou-se com', nome, 'em 2012.');
+console.log('Maria engravidou de', nome, 'em 2015.');
+console.log('O nome do filho de', nome, 'é Eduardo.');
