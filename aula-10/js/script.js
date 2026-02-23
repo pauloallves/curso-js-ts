f2a04251828c60fb859d1acc59ae98eabf48e66e
Paulo Henrique <paulohenriquesantos280@gmail.com>
2026-02-23 20:11:36 -0300

    Segundo commit da aula de hoje

--- aula-10/js/script.js
+++ aula-10/js/script.js
@@ -1,1 +1,12 @@
-alert('Com a nossa mensagem');
+let num1 = parseInt(prompt('Digite o primeiro número'));
+let num2 = parseInt(prompt('Digite o segundo número'));
+
+//num1 = Number(num1)
+//num2 = Number(num2)
+
+let soma = num1 + num2;
+
+//alert('O resultado da soma dos dois números digitador é' + ' ' + soma);
+alert(`O resultado da soma dos dois números digitador é  ${soma}`);
+
+console.log(soma);
---
+++ aula-11/exercicio.js
@@ -0,0 +1,13 @@
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+//let varP = varA;
+//varA = varB;
+//varB = varC;
+//varC = varP;
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC);
+
