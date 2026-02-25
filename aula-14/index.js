2d1e35bf2c40653e78919a898c46fcd649bba4a7
Paulo Henrique <paulohenriquesantos280@gmail.com>
2026-02-25 20:33:43 -0300

    Segundo commit da aula do dia 25/02/2026

---
+++ aula-14/index.js
@@ -0,0 +1,51 @@
+// IEEE 754-2008
+
+//let num1 =10.25;    //number
+//let num2 =2;  //numberr
+
+let num1 = 0.7;    //number
+let num2 = 0.1;  //numberr
+
+/*
+num1 += num2;    //0.8
+num1 += num2;    //0.9
+num1 += num2;    //1.0
+num1 += num2;    //1.1
+num1 += num2;    //1.2
+num1 += num2;    //1.3
+num1 += num2;    //1.4
+num1 += num2;    //1.5
+num1 += num2;    //1.6
+num1 += num2;    //1.7
+num1 += num2;    //1.8
+num1 += num2;    //1.9
+num1 += num2;    //2.0
+*/
+
+//num1 = ((num1 * 100) + (num2 * 100)) / 100;   //0.8
+//num1 = ((num1 * 100) + (num2 * 100)) / 100;   //0.9
+//num1 = ((num1 * 100) + (num2 * 100)) / 100;   //1.00
+
+num1 = Number(num1.toFixed(2));
+
+num2 = Number(num2.toFixed(2));
+
+
+//num1 = parseFloat(num1.toFixed(2));
+//num1 = Number(num1.toFixed(2));
+console.log(Number.isInteger(num1));
+console.log(Number.isInteger(num2));
+
+//console.log(num1.toString() + num2); convertando um number provisoriamente em string sem alterar o valor real
+//num1 = num1.toString(); converter de fomar definitiva um number em string
+
+// console.log(typeof num1); Verificando o tipo de dado
+//console.log(num1.toString(2)); //representação binária do valor que está na variável
+//console.log(num1.toFixed(2)) //arrendondar número variável sem altear o valor real que está na variável.
+//console.log(Number.isInteger(num1)); //verificando se o valor que está na variável é um inteiro
+//let temp = num1 * '5';
+//console.log(Number.isNaN(temp));
+console.log(num1);
+console.log(num2);
+
+console.log(num1 + num2);
