beda514d7f70e2a6028e9905212de9d42681be9c
Paulo Henrique <paulohenriquesantos280@gmail.com>
2026-02-26 07:13:35 -0300

    primeiro commit da aula do dia 26/02/2026

---
+++ aula-15/index.js
@@ -0,0 +1,23 @@
+let num1 = 9;
+//let num2 = Math.floor(num1); //Arrendonda pra baixo
+//let num2 = Math.ceil(num1); Arredonda pra cima
+//let num2 = Math.round(num1); //arrendonda para o número inteiro amais próximo
+
+//console.log(Math.max(1,2,5,500,501,100)); //Me devolve o maior número dessa sequencia
+
+//console.log(Math.min(1,2,5,500,501,100)); //Me devolve o menor número dessa sequencia
+
+console.log(Math.random()); //Gera numeros pseudo aletaório entre 0 e 1, mas não inclui o numeral 1
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); //sorteando numeros aleatrio entre 10 e 5
+console.log(aleatorio);
+
+console.log(Math.PI); //pegando o valor de PI
+
+console.log(Math.pow(2, 10)); //Potenciação
+console.log(2 ** 10); //Potenciação
+
+console.log(num1 ** (1/2)); //obter a raiz quadrada de um número
+console.log(num1 ** 0.5); //obter a raiz quadrada de um número
+
+console.log(100 / 0); //em javascript é possível dividir um numero por zero
+
