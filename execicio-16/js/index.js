8620c45e083e797bcaf5447ac2cd79e04e7c02a7
Paulo Henrique <paulohenriquesantos280@gmail.com>
2026-02-26 21:26:02 -0300

    Segundo commit da aula do dia 26/02/2026

--- execicio-16/js/index.js
+++ execicio-16/js/index.js
@@ -4,4 +4,9 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p\>Seu número - 2 é: ${numero -2}</p>`;
+texto.innerHTML += `<p\>Raiz quadrada: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p\>O número ${numero} é um iteiro? ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p\>O número ${numero} é um NaN? ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p\>Seu número ${numero} arendondado para baixo é: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p\>Seu número ${numero} arendondado para cima é: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p\>Seu número ${numero} com duas casas decimais é: ${numero.toFixed(2)}</p>`;
